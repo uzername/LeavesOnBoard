866e37176dd5c6d64c0ed04db320d0cf1b6e19b4
uzername <ivanpost777@gmail.com>
2022-02-19 16:59:25 +0200

    daily commit. Random amount of leaves. Random rotation of leaves. Edit on SVG

--- JS/main.js
+++ JS/main.js
@@ -4,10 +4,15 @@ function initMain() {
     var drawCanvasWidth = parseFloat(getComputedStyle(mainHTMLElementBoard, null).width.replace("px", ""));
     var drawCanvasHeight = parseFloat(getComputedStyle(mainHTMLElementBoard, null).height.replace("px", ""));    
     var leafCount = getRandomIntBetween(20,200);
+    //leafCount = 0;
     for(var i=0; i<leafCount; i++) {
-       var XCoord = getRandomIntBetween(0, drawCanvasWidth-50);
-       var YCoord = getRandomIntBetween(0, drawCanvasHeight-50);
-       addLeafRandomly(YCoord, XCoord, mainHTMLElementBoard,i);
+       var kindOfLeaf = getRandomIntBetween(1,5);
+       var wdthLeaf = getRandomIntBetween(50,100);
+       var hghtLeaf = wdthLeaf;
+       var XCoord = getRandomIntBetween(0, drawCanvasWidth-wdthLeaf);
+       var YCoord = getRandomIntBetween(0, drawCanvasHeight-hghtLeaf);
+       var rotationDegrees = getRandomIntBetween(1,359);
+       addLeafRandomly(XCoord, YCoord, wdthLeaf, hghtLeaf, mainHTMLElementBoard,i,kindOfLeaf,rotationDegrees);
     }
 }
 
@@ -23,22 +28,29 @@ function setWidth(el, val) {
     if (typeof val === "string") el.style.width = val;
     else el.style.width = val + "px";
 }
-
-function addLeafRandomly(xPosition, yPosition, elementToAdd, indexIndex) {
+/// Add leaf randomly
+/// xPosition, yPosition - left and right offset of upper left point, assuming position:absolute. Integer.
+/// inWdth, inHght - width and height, for transformation
+/// elementToAdd - root DOM element for adding
+/// indexIndex - ID of leaf
+/// inKindOfLeaf - ordinal number, kind of leaf (1 to 5, by now: leaf1 ... leaf5)
+/// inDegrees - rotation degrees 
+function addLeafRandomly(xPosition, yPosition, inWdth, inHght, elementToAdd, indexIndex, inKindOfLeaf, inDegrees) {
   var obj = document.createElement('object');
   obj.setAttribute("type", "image/svg+xml");
-  obj.setAttribute("data","CONTENTFINAL/leaf1.svg");
-  obj.setAttribute("width","50");
-  obj.setAttribute("height","50");
+  obj.setAttribute("data","CONTENTFINAL/leaf"+inKindOfLeaf.toString()+".svg");
+  obj.setAttribute("width",inWdth.toString());
+  obj.setAttribute("height",inHght.toString());
   var used_xPosition=xPosition; var used_yPosition=yPosition;
-  obj.setAttribute("style","position: absolute; top: "+xPosition.toString()+"px; left:"+yPosition.toString()+"px;")
+  obj.setAttribute("style","position: absolute; top: "+yPosition.toString()+"px; left:"+xPosition.toString()+"px;"+
+  "transform: rotate("+inDegrees.toString()+"deg);")
+  
   obj.setAttribute("id","leaf"+indexIndex.toString());
   elementToAdd.appendChild(obj);
 }
 
 function testClick(whatCatched) {
     // assume that whatCatched is a Window
-    // console.log(whatCatched);
-    //console.log(whatCatched.frameElement);
+
     console.log(whatCatched.frameElement.id);
 }
